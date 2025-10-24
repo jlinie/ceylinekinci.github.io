3b1a38e8169e695c5418fb8d9a50aed357fa663d
jlinie <ceylinekinci@sabanciuniv.edu>
2025-10-24 13:34:36 +0300

    chore: set basePath/assetPrefix for project page

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const repo = "ceylinekinci.github.io";
+
 const nextConfig: NextConfig = {
   output: "export",          
   images: { unoptimized: true },
